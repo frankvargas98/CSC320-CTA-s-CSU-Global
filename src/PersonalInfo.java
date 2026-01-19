c629f3f8aa3d0dd6679cfb48c76d3af22062bb01
Frank <frankvargas98@gmail.com>
2026-01-18 21:39:30 -0700

    Initial Commit

---
+++ src/PersonalInfo.java
@@ -0,0 +1,19 @@
+/* This is the main program that will collect and display personal info
+ */
+public class PersonalInfo{
+    public static void main(String[] args) {
+        String firstName = "Frank";
+        String lastName = "Vargas";
+        String streetAddress = "555 17th St";
+        String city = "Denver, CO";
+        String zipCode = "80202";
+
+                //Below are the instructions to print out each line
+        System.out.print(firstName + " ");
+        System.out.println(lastName);
+        System.out.println(streetAddress);
+        System.out.print(city + " ");
+        System.out.println(zipCode);
+
+    }
+}
